eab6c65f9b8664ad21dc0f22fd7c9cfacad9ac85
LexKeivi <lex.keivi@mail.ru>
2022-10-29 00:08:12 +0500

    Add shoot ball on direction

---
+++ Assets/ShootOnDirection.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ShootOnDirection : MonoBehaviour
+{
+    [SerializeField] private GameObject _ball;
+    [SerializeField] private float _force = 1f;
+    private Rigidbody _ballRigidbody;
+
+    private Vector2 _mousePositionOnScreen;
+    private Camera _camera;
+
+    void Start()
+    {
+        _ballRigidbody = GetComponent<Rigidbody>();
+        _camera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            var _mousePosition = Input.mousePosition;
+            _mousePositionOnScreen = _camera.ScreenToWorldPoint(_mousePosition);
+        }
+
+        if (Input.GetButtonUp("Fire1") && _mousePositionOnScreen.y > _ball.transform.position.y)
+        {
+            _ballRigidbody.velocity = (_mousePositionOnScreen - (Vector2)_ball.transform.position).normalized * _force;
+        }
+    }
+}
